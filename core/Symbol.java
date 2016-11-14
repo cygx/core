ae040dc9641ac1b6dc6f7349a1401f792c0c54f1
cygx <cggaertner@googlemail.com>
2016-11-14 20:43:26 +0100

    give symbols a unique id

--- core/Symbol.java
+++ core/Symbol.java
@@ -1,12 +1,21 @@
 package core;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class Symbol implements Value {
+public final class Symbol implements Value {
+    private static final AtomicInteger nextId = new AtomicInteger();
+
+    public final int id;
+
+    public Symbol() {
+        this.id = nextId.getAndIncrement();
+    }
+
     public Symbol type() {
         return this;
     }
 
     @Override
     public String toString() {
-        return String.format("@%x", hashCode());
+        return String.format("@%x", id);
     }
 }
